b451ff55e0b72cc464acc7342e16ca64fb1d8e5b
Harshana Praveen <harshana6681@gmail.com>
2026-08-17 22:12:37 +0530

    fixing minor error

--- src/main.tsx
+++ src/main.tsx
@@ -2,17 +2,20 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
+import { ThemeProvider } from '@mui/material/styles';
+import { CssBaseline } from '@mui/material';
 import { store } from './app/store';
+import { theme } from './theme/theme';
 import App from './App';
 import './index.css';
-import { theme } from './theme/theme';
-import { ThemeProvider } from '@emotion/react';
-import { CssBaseline } from '@mui/material';
 
 createRoot(document.getElementById('root')!).render(
-  // Wrap the Provider content in main.tsx
-  <ThemeProvider theme={theme}>
-    <CssBaseline />
-    <App />
-  </ThemeProvider>,
+  <StrictMode>
+    <Provider store={store}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </Provider>
+  </StrictMode>,
 );
